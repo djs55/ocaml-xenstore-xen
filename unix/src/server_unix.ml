b8f6b59611657ed8067aa0da2cddde2fe79690fa
David Scott <dave.scott@eu.citrix.com>
2013-09-12 16:49:15 +0100

    unix: improve the command-line parsing and logging
    We perform command-line parsing before we daemonize, so early error
    messages can be printed to the console. If we daemonize, then we
    log via syslog; otherwise we print to stdout.
    
    Add 2 new command-line options:
      -daemon : if we want to daemonize (default false)
      -pidfile <file> : location to write the pidfile
    
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- unix/src/server_unix.ml
+++ unix/src/server_unix.ml
@@ -22,30 +22,32 @@ module DomainServer = Xenstore_server.Xs_server.Server(Xs_transport_xen)
 
 let syslog = Lwt_log.syslog ~facility:`Local3 ()
 
+let daemon = ref false
+
 let rec logging_thread logger =
 	lwt lines = Xenstore_server.Logging.get logger in
 	lwt () = Lwt_list.iter_s
 			(fun x ->
-				lwt () = Lwt_log.log ~logger:syslog ~level:Lwt_log.Notice x in
+				lwt () =
+					if !daemon
+					then Lwt_log.log ~logger:syslog ~level:Lwt_log.Notice x
+					else Lwt_io.write_line Lwt_io.stdout x in
 				return ()
 			) lines in
 	logging_thread logger
+
+let pidfile = ref "/var/run/xenstored.pid"
 
 let main () =
 	debug "Unix xenstored starting";
 	let (_: 'a) = logging_thread Xenstore_server.Logging.logger in
 	let (_: 'a) = logging_thread Xenstore_server.Logging.access_logger in
 
-	let pidfile = "/var/run/xenstored.pid" in
-	debug "Writing pidfile %s" pidfile;
-	(try Unix.unlink pidfile with _ -> ());
+	debug "Writing pidfile %s" !pidfile;
+	(try Unix.unlink !pidfile with _ -> ());
 	let pid = Unix.getpid () in
-	lwt _ = Lwt_io.with_file pidfile ~mode:Lwt_io.output (fun chan -> Lwt_io.fprintlf chan "%d" pid) in
+	lwt _ = Lwt_io.with_file !pidfile ~mode:Lwt_io.output (fun chan -> Lwt_io.fprintlf chan "%d" pid) in
 
-	Arg.parse
-		[ "-path", Arg.Set_string Xs_transport_unix.xenstored_socket, Printf.sprintf "Unix domain socket to listen on (default %s)" !Xs_transport_unix.xenstored_socket ]
-		(fun _ -> ())
-		"User-space xenstore service";
 	let (a: unit Lwt.t) = UnixServer.serve_forever () in
 	debug "Started server on unix domain socket";
 	let (b: unit Lwt.t) = DomainServer.serve_forever () in
@@ -57,5 +59,17 @@ let main () =
 	return ()
 
 let _ =
-	Lwt_daemon.daemonize ();
+	Arg.parse [
+		"-path", Arg.Set_string Xs_transport_unix.xenstored_socket, Printf.sprintf "Unix domain socket to listen on (default %s)" !Xs_transport_unix.xenstored_socket;
+		"-pidfile", Arg.Set_string pidfile, Printf.sprintf "File to write PID (default %s)" !pidfile;
+		"-daemon", Arg.Set daemon, Printf.sprintf "True if we should daemonize (default %b)" !daemon;
+		 ]
+		(fun _ -> ())
+		"User-space xenstore service";
+	let dir_needed = Filename.dirname !Xs_transport_unix.xenstored_socket in
+	if not(Sys.file_exists dir_needed && (Sys.is_directory dir_needed)) then begin
+		Printf.fprintf stderr "The directory where the socket should be created (%s) doesn't exist.\n" dir_needed;
+		exit 1;
+	end;
+	if !daemon then Lwt_daemon.daemonize ();
 	Lwt_main.run (main ())
